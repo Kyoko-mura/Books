fa8e7059c6df74e1955908f15ea36d7a6e388ae0
Kyoko-mura <today.mura@gmail.com>
2020-03-14 20:29:16 +0900

    終了

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'books#top'
+  resources :books
+  patch 'books/:id' => 'books#update', as: 'update_books'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
